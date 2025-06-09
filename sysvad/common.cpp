de325ce10e1dac20c4b4e6ce55b608be1860c47d
taj0207 <taj0207@gmail.com>
2025-06-08 20:06:50 -0700

    Initialize DEVICE_RELATIONS allocation

--- sysvad/common.cpp
+++ sysvad/common.cpp
@@ -2421,6 +2421,10 @@ CAdapterCommon::UpdatePowerRelations(_In_ PIRP Irp)
         goto Exit;
     }
 
+    // Ensure the new structure is initialized so Count starts at zero and
+    // Objects contains valid pointers only when explicitly set.
+    RtlZeroMemory(newRelations, size);
+
     //
     // If there was an existing device relations structure, copy
     // the entries to the new structure.
